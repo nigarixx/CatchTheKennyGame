132c61b6fe859c70677effb15f95b971c0a5e3e0
nigarixx <alaskarovanigar@gmail.com>
2024-02-16 21:31:36 +0400

    Initial Commit

---
+++ CatchTheKennyGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CatchTheKennyGame
+//
+//  Created by Nigar Alaskarova on 16.02.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
